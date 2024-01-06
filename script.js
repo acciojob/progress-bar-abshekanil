3c63eaa64674c29a6b459b2a24da3496de5f248c
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-06 17:38:32 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,28 +1,30 @@
 //your JS code here. If required.
 document.addEventListener("DOMContentLoaded", function(){
 	const circles = document.querySelectorAll(".circle");
-	const prevBtn = document.getElementsById("prevBtn");
-	const nextBtn = document.getElementsById("nextBtn");
+	const prevBtn = document.getElementById("prevBtn");
+	const nextBtn = document.getElementById("nextBtn");
 
 	let currentIndex = 0;
 
 	updateButtonState();
 
-	prevBtn.addEventListener("click" function(){
+	prevBtn.addEventListener("click", function(){
 		if(currentIndex > 0)
 		{
 			currentIndex--;
 			updateButtonState();
 		}
-	});
+	});  
 
-	nextBtn.addEventListener("click" function(){
+
+	nextBtn.addEventListener("click", function(){
 		if(currentIndex < circles.length - 1)
 		{
 			currentIndex++;
 			updateButtonState();
 		}
-	});
+	}); 
+
 		function updateButtonState(){
 			circles.forEach((circle, index) =>{
 				if(index === currentIndex)
