343f31a7b02e58f558c8921cc27b35e28e3b536b
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-06 18:01:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,44 +1,47 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function(){
-	const circles = document.querySelectorAll(".circle");
-	const prevBtn = document.getElementById("prev");
-	const nextBtn = document.getElementById("next");
-	const lines = document.querySelectorAll(".line");
+document.addEventListener("DOMContentLoaded", function () {
+    const circles = document.querySelectorAll(".circle");
+    const prevBtn = document.getElementById("prev");
+    const nextBtn = document.getElementById("next");
+    const lines = document.querySelectorAll(".line");
 
-	let currentIndex = 0;
+    let currentIndex = 0;
 
-	updateButtonState();
+    updateButtonState();
 
-	prevBtn.addEventListener("click", function(){
-		if(currentIndex > 0)
-		{
-			currentIndex--;
-			updateButtonState();
-		}
-	});  
+    prevBtn.addEventListener("click", function () {
+        if (currentIndex > 0) {
+            currentIndex--;
+            updateButtonState();
+        }
+    });
 
+    nextBtn.addEventListener("click", function () {
+        if (currentIndex < circles.length - 1) {
+            circles[currentIndex].classList.add("previous");
+            currentIndex++;
+            updateButtonState();
+        }
+    });
 
-	nextBtn.addEventListener("click", function(){
-		if(currentIndex < circles.length - 1)
-		{
-			circles[currentIndex].classList.add("previous");
-			currentIndex++;
-			updateButtonState();
-		}
-	}); 
+    function updateButtonState() {
+        circles.forEach((circle, index) => {
+            if (index === currentIndex) {
+                circle.classList.add("active");
+            } else {
+                circle.classList.remove("active");
+            }
 
-		function updateButtonState(){
-			circles.forEach((circle, index) =>{
-				if(index === currentIndex)
-				{
-					circle.classList.add("active");
-				}
-				else{
-					circle.classList.remove("active", "previous");
-				}
-			});
+			if(index < currentIndex)
+			{
+				circle.classList.add("previous");
+			}
+			else
+			{
+				circle.classList.remove("previous");
+			}
+        });
 
-			lines.forEach((line, index) => {
+        lines.forEach((line, index) => {
             if (index < currentIndex) {
                 line.classList.add("active");
             } else {
@@ -46,7 +49,7 @@ document.addEventListener("DOMContentLoaded", function(){
             }
         });
 
-			prevBtn.disabled = currentIndex === 0;
-			nextBtn.disabled = currentIndex === circles.length - 1;
-		}
+        prevBtn.disabled = currentIndex === 0;
+        nextBtn.disabled = currentIndex === circles.length - 1;
+    }
 });
