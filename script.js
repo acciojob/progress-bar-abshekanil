86929b856748204800bbf1c289500ed91668c892
Abhishek Anil <141896050+abshekanil@users.noreply.github.com>
2024-01-06 17:27:33 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function(){
+	const circles = document.querySelectorAll(".circle");
+	const prevBtn = document.getElementsById("prevBtn");
+	const nextBtn = document.getElementsById("nextBtn");
+
+	let currentIndex = 0;
+
+	updateButtonState();
+
+	prevBtn.addEventListener("click" function(){
+		if(currentIndex > 0)
+		{
+			currentIndex--;
+			updateButtonState();
+		}
+	});
+
+	nextBtn.addEventListener
+});
